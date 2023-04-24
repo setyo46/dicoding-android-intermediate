4fcc2c2b9a50a18e0f972aa0a73705aa4829ef66
unknown <setyoadisasono@gmail.com>
2023-04-24 22:16:50 +0700

    Latihan StackView Widget

---
+++ MyStackWidget/app/src/main/java/com/setyo/mystackwidget/StackWidgetService.kt
@@ -0,0 +1,11 @@
+package com.setyo.mystackwidget
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService: RemoteViewsService() {
+
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory =
+        StackRemoteViewsFactory(this.applicationContext)
+
+}
