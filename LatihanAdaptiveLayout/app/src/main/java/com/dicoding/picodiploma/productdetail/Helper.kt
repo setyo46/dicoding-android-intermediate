68be9ce9c1e5cc37ff40d5482d2566c41669f8ff
unknown <setyoadisasono@gmail.com>
2023-04-30 21:40:17 +0700

    menambahkan format curency

--- LatihanAdaptiveLayout/app/src/main/java/com/dicoding/picodiploma/productdetail/Helper.kt
+++ LatihanAdaptiveLayout/app/src/main/java/com/dicoding/picodiploma/productdetail/Helper.kt
@@ -7,11 +7,37 @@ import java.util.Date
 import java.util.Locale
 
 fun String.withNumberingFormat(): String {
-        return NumberFormat.getNumberInstance().format(this.toDouble())
+    return NumberFormat.getNumberInstance().format(this.toDouble())
+}
+
+fun String.withDateFormat(): String {
+    val format = SimpleDateFormat("dd/MM/yyyy", Locale.US)
+    val date = format.parse(this) as Date
+    return DateFormat.getDateInstance(DateFormat.FULL).format(date)
+}
+
+fun String.withCurrencyFormat(): String {
+    val rupiahExchangeRate = 12495.95
+    val euroExchangeRate = 0.88
+
+    var priceOnDollar = this.toDouble() / rupiahExchangeRate
+
+    var mCurrencyFormat = NumberFormat.getCurrencyInstance()
+    val deviceLocale = Locale.getDefault().country
+    when {
+        deviceLocale.equals("ES") -> {
+            priceOnDollar *= euroExchangeRate
+        }
+        deviceLocale.equals("ID") -> {
+            priceOnDollar *= rupiahExchangeRate
+        }
+        else -> {
+            mCurrencyFormat = NumberFormat.getCurrencyInstance(Locale.US)
+        }
     }
 
-    fun String.withDateFormat(): String {
-        val format = SimpleDateFormat("dd/MM/yyyy", Locale.US)
-        val date = format.parse(this) as Date
-        return DateFormat.getDateInstance(DateFormat.FULL).format(date)
-    }
+    return mCurrencyFormat.format(priceOnDollar)
+}
+
+
+
