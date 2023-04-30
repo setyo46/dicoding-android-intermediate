1856966730de756aa59327fd721a0124290e6e15
unknown <setyoadisasono@gmail.com>
2023-04-30 20:26:04 +0700

    Menambahkan Format Penomoran

---
+++ LatihanAdaptiveLayout/app/src/main/java/com/dicoding/picodiploma/productdetail/Helper.kt
@@ -0,0 +1,8 @@
+package com.dicoding.picodiploma.productdetail
+
+import java.text.NumberFormat
+
+    fun String.withNumberingFormat(): String {
+        return NumberFormat.getNumberInstance().format(this.toDouble())
+
+    }
