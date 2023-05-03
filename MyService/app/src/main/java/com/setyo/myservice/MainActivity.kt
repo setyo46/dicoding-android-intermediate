0c1198e8167a781fc0de99308f80de55761ede17
unknown <setyoadisasono@gmail.com>
2023-05-03 14:06:46 +0700

    membuat service foreground

--- MyService/app/src/main/java/com/setyo/myservice/MainActivity.kt
+++ MyService/app/src/main/java/com/setyo/myservice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.setyo.myservice
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.setyo.myservice.databinding.ActivityMainBinding
@@ -13,14 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+        val foregroundServiceIntent = Intent(this, MyBackgroundService::class.java)
 
-        binding.btnStartBackgroundService.setOnClickListener {
-            startService(serviceIntent)
+        binding.btnStartForegroundService.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(foregroundServiceIntent)
+            }
+            startService(foregroundServiceIntent)
         }
 
-        binding.btnStopBackgroundService.setOnClickListener {
-            stopService(serviceIntent)
+        binding.btnStopForegroundService.setOnClickListener {
+            stopService(foregroundServiceIntent)
         }
     }
 }
